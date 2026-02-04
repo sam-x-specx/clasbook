b9024b69351c7aad17d673438c3be542a9a7dd58
Samharison <sam.sahoos20050@gmail.com>
2026-02-04 23:13:17 +0530

    Update database settings to use environment variables

--- classbook/settings.py
+++ classbook/settings.py
@@ -165,11 +165,11 @@ SOCIALACCOUNT_PROVIDERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'classbook',
-        'USER': 'postgres',
-        'PASSWORD': 'Samx@7sam', # PGAdmin password
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
