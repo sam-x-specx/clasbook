870941585ab94d33cea1b7ccf33dc2c323539f13
Samharison <sam.sahoos20050@gmail.com>
2026-02-05 10:01:47 +0530

    Refactor DATABASES settings for Django configuration

--- classbook/settings.py
+++ classbook/settings.py
@@ -169,21 +169,21 @@ SOCIALACCOUNT_PROVIDERS = {
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,          # keep connections alive (good for Render)
-        ssl_require=True           # required on Render
-    )
-}
-
 # DATABASES = {
-#         'default': {
+#     'default': dj_database_url.config(
+#         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+#         conn_max_age=600,          # keep connections alive (good for Render)
+#         ssl_require=True           # required on Render
+#     )
+# }
+
+DATABASES = {
+        'default': {
             
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': BASE_DIR / 'db.sqlite3',
-    #     }
-    # }
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
         # 'ENGINE': 'django.db.backends.postgresql',
         # 'NAME': 'classbook',
