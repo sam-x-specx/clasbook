98254f96509b64c3cbd5a0bd90e25b6eb6daff0c
Samharison <sam.sahoos20050@gmail.com>
2026-02-05 08:49:47 +0530

    Add STATIC_URL and STATIC_ROOT settings

--- classbook/settings.py
+++ classbook/settings.py
@@ -20,6 +20,9 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
