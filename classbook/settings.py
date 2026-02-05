8b556293cde9e3dc6afb0cda2c985e3dc3adfb79
Samharison <sam.sahoos20050@gmail.com>
2026-02-05 09:04:58 +0530

    Change os.environ to os.getenv for environment vars

--- classbook/settings.py
+++ classbook/settings.py
@@ -184,11 +184,11 @@ DATABASES = {
         # 'PORT': '5432',
 
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
         }
 }
 
